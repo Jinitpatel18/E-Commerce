2e6c3e74619a262f05b9a0c869659322b5e7b9a1
Jinitpatel18 <jinitpatel188@gmail.com>
2026-05-04 10:56:21 +0530

    Edit app.js

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ const app = express();
 app.set('trust proxy', 1)
 app.use(helmet());
 app.use(cors({
-    origin: ['http://localhost:5173',
-        'https://tera-frontend.vercel.app'],
+    origin: ['https://frontend-nine-indol-16.vercel.app',
+        'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
 }))
